d4fa998e94c82c298a7760d212124a69519d2601
wander23 <57674214+wander23@users.noreply.github.com>
2022-03-25 20:54:11 +0700

    complete paging

--- src/components/user-manage/user_manageController.js
+++ src/components/user-manage/user_manageController.js
@@ -3,8 +3,36 @@ const service = require('./user_manageService');
 
 // Render admin manage
 exports.renderAdminManage = async (req, res) => {
-    const admin_info = await service.getAdminInfo();
-    res.render("user-manage/views/admin_manage", { active: { AdminManage: true }, page: "Admin manage", admin_info });
+    const admins = await service.getAdminInfo();
+
+    const page = parseInt(req.query.page) || 1;
+    const limit = 5;
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const result = {};
+
+    console.log('end index:', endIndex);
+    console.log('admins:',admins.length);
+
+    if(endIndex < admins.length) {
+        result.next = page + 1;
+    }
+    else {
+        result.disableNext = 'pointer-events: none;';
+    }
+
+    if(startIndex > 0) {
+        result.prev = page - 1;
+    }
+    else {
+        result.disablePrev = 'pointer-events: none;';
+    }
+    result.page = page;
+    result.admins = admins.slice(startIndex, endIndex);
+
+    res.render("user-manage/views/admin_manage", { active: { AdminManage: true }, page: "Admin manage", result});
 };
 
 // Render admin manage
