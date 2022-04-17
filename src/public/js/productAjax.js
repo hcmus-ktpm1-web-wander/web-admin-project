8e3132dcba0add94b03bf3f9b30b7c4352e5627c
QuannBluee <quannguyenthanh558@gmail.com>
2022-04-17 07:08:21 +0700

    fix error cant add color in AddProduct and Update checkbox sz

--- src/public/js/productAjax.js
+++ src/public/js/productAjax.js
@@ -1,4 +1,4 @@
-function getProductsByFilter(page, category=null, brand=null, min_price = '0', max_price= '999999') {
+function getProductsByFilter(page, category = null, brand = null, min_price = '0', max_price = '999999') {
     const sort_type = $('#sort-dropdown').find(":selected").val();
     if (min_price == '')
         min_price = '0'
@@ -7,9 +7,9 @@ function getProductsByFilter(page, category=null, brand=null, min_price = '0', m
 
 
     if (category == null || category.length == 0)
-        category=null
+        category = null
     if (brand == null || brand.length == 0)
-        brand=null
+        brand = null
 
     fetch(`/api/product?sort=${sort_type}&page=${page}&category=${JSON.stringify(category)}&brand=${JSON.stringify(brand)}&min=${min_price}&max=${max_price}`, {
         method: "GET"
@@ -61,104 +61,119 @@ function getProductsByFilter(page, category=null, brand=null, min_price = '0', m
     });
 }
 
-function test(field)
-{
+function test(field) {
     console.log(field)
 
 }
 
 
-function checkAll()
-{
+function checkAll() {
     let category_filter = []
     let brand_filter = []
     let min_price = 0;
     let max_price = 0;
 
     const size_check_boxes = $('.categories input[type=checkbox]')
-    size_check_boxes.each(function (){
+    size_check_boxes.each(function () {
         const id = $(this).attr('id')
-        if($(this).is(':checked'))
+        if ($(this).is(':checked'))
             category_filter.push(id)
     })
 
     const brand_size_checkboxes = $('.brand input[type=checkbox]')
-    brand_size_checkboxes.each(function (){
+    brand_size_checkboxes.each(function () {
         const id = $(this).attr('id')
-        if($(this).is(':checked'))
+        if ($(this).is(':checked'))
             brand_filter.push(id)
     })
 
     const price_inputs = $('.price input[type=number]')
-    price_inputs.each(function (){
+    price_inputs.each(function () {
         const name = $(this).attr('name')
         const value = $(this).val()
-        if(name == 'min-price')
+        if (name == 'min-price')
             min_price = value
         else
             max_price = value
     })
 
-    return {category: category_filter, brand: brand_filter, min_price: min_price, max_price: max_price}
+    return { category: category_filter, brand: brand_filter, min_price: min_price, max_price: max_price }
 }
 
-function Init()
-{
+function Init() {
 
     const filter_form = $('#collapseOne')
     const expand_btn = $('a')
-    expand_btn.on('click', function (){
-        if (filter_form.attr('class') == 'collapse')
-        {
-            filter_form.attr('class','collapsing')
-            setTimeout(function (){
-                filter_form.attr('class','collapse show')
-            },50)
+    expand_btn.on('click', function () {
+        if (filter_form.attr('class') == 'collapse') {
+            filter_form.attr('class', 'collapsing')
+            setTimeout(function () {
+                filter_form.attr('class', 'collapse show')
+            }, 50)
         }
-        else
-        {
-            filter_form.attr('class','collapsing')
-            setTimeout(function (){
-                filter_form.attr('class','collapse')
-            },50)
+        else {
+            filter_form.attr('class', 'collapsing')
+            setTimeout(function () {
+                filter_form.attr('class', 'collapse')
+            }, 50)
         }
 
     })
 
     const checkboxes = $('input[type=checkbox]')
-    checkboxes.each(function (){
-        $(this).on("input", function (){
-            const result=checkAll()
-            getProductsByFilter(1,result.category,result.brand,result.min_price,result.max_price)
+    checkboxes.each(function () {
+        $(this).on("input", function () {
+            const result = checkAll()
+            getProductsByFilter(1, result.category, result.brand, result.min_price, result.max_price)
 
         })
     })
 
     const price_inputs = $('.price input[type=number]')
-    price_inputs.each(function (){
+    price_inputs.each(function () {
         const name = $(this).attr('name')
         if (name == 'min-price')
-            $(this).attr('value','0')
+            $(this).attr('value', '0')
         else
-            $(this).attr('value','999999')
+            $(this).attr('value', '999999')
 
-        $(this).on("input", function (){
-            const result=checkAll()
-            getProductsByFilter(1,result.category,result.brand,result.min_price,result.max_price)
+        $(this).on("input", function () {
+            const result = checkAll()
+            getProductsByFilter(1, result.category, result.brand, result.min_price, result.max_price)
         })
     })
 
     const select = $('#sort-dropdown')
-    select.on("input", function (){
-        const result=checkAll()
-        getProductsByFilter(1,result.category,result.brand,result.min_price,result.max_price)
+    select.on("input", function () {
+        const result = checkAll()
+        getProductsByFilter(1, result.category, result.brand, result.min_price, result.max_price)
     })
 }
 
 
 window.onload = function () {
-    const category = ['Bags','Accessories']
+    const category = ['Bags', 'Accessories']
     getProductsByFilter(1)
     Init()
 }
 
+// var numberadd = {{ color.length }} || 0;
+// var div_add_color = document.getElementById('add-color');
+// const add_div = "<div id='color-" + numberadd + "-wrap' class='position-relative mx-1 mb-2'> <input type='color' class='form-control-color' name='color' id=\"color-" + numberadd + "\"> <a id=\"color-" + numberadd + "-minus\"  onmousedown='removeColor(\"color-" + numberadd + "\")'> <i class='fa fa-minus-circle position-absolute' style=' z-index: 1; right:-5px; top:-4px;' aria-hidden='true'> </i > </a > </div >";
+
+// function addColor() {
+//     const _id = 'color-' + numberadd;
+//     const _id_minus = _id + '-minus';
+//     console.log("add color");
+
+//     div_add_color.innerHTML += add_div;
+
+//     numberadd++;
+// }
+
+// function removeColor(id) {
+//     console.log("remove", id)
+//     document.getElementById(id).remove();
+//     document.getElementById(id + '-minus').remove();
+//     document.getElementById(id + '-wrap').remove();
+// }
