40e657be1350c5c112aca5f9baf063b44f9602cf
QuannBluee <quannguyenthanh558@gmail.com>
2022-04-25 08:11:49 +0700

    fix nav bar UI

--- src/components/order/orderController.js
+++ src/components/order/orderController.js
@@ -6,9 +6,9 @@
  */
 exports.renderOrder = async (req, res) => {
     try {
-        res.render("order/views/order", {active: {Order: true}, page: "Order"});
+        res.render("order/views/order", { layout: "/order/views/order_layout", active: { Order: true }, page: "Order" });
     } catch (e) {
-        res.status(500).json({message: e.message});
+        res.status(500).json({ message: e.message });
     }
 };
 
